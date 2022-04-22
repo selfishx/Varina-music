d75b996f7bad97690c2b63067f15b43efef8af23
selfishx <97510714+selfishx@users.noreply.github.com>
2022-04-22 11:58:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ async def start_bot():
     await idle()
     print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()
-    await bot.join_chat("TrickyAbhii_Op")
+    await bot.join_chat("CLASSY_NETWORK")
     await Herox.send_message(
                SUPPORT,
             "<b>Congrats!! Music Bot has started successfully!</b>",
